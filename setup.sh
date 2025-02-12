fd875c91be6107322c28e28413eb5399b87eb3e8
Ostad <yasinjjjv@gmail.com>
2025-02-13 01:27:43 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -66,26 +66,25 @@ case $choice in
         
         # Edit sysctl.conf
         echo "Editing /etc/ufw/sysctl.conf and /etc/sysctl.conf..."
-        SYSCTL_SETTINGS="
-        fs.file-max = 51200
-        net.core.rmem_max = 67108864
-        net.core.wmem_max = 67108864
-        net.core.netdev_max_backlog = 250000
-        net.core.somaxconn = 4096
-        net.ipv4.tcp_syncookies = 1
-        net.ipv4.tcp_tw_reuse = 1
-        net.ipv4.tcp_tw_recycle = 0
-        net.ipv4.tcp_fin_timeout = 30
-        net.ipv4.tcp_keepalive_time = 1200
-        net.ipv4.ip_local_port_range = 10000 65000
-        net.ipv4.tcp_max_syn_backlog = 8192
-        net.ipv4.tcp_max_tw_buckets = 5000
-        net.ipv4.tcp_fastopen = 3
-        net.ipv4.tcp_mem = 25600 51200 102400
-        net.ipv4.tcp_rmem = 4096 87380 67108864
-        net.ipv4.tcp_wmem = 4096 65536 67108864
-        net.ipv4.tcp_mtu_probing = 1
-        net.ipv4.tcp_congestion_control = hybla"
+        SYSCTL_SETTINGS="fs.file-max = 51200
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 4096
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_tw_recycle = 0
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_congestion_control = hybla"
         
         echo "$SYSCTL_SETTINGS" | sudo tee /etc/ufw/sysctl.conf > /dev/null
         echo "$SYSCTL_SETTINGS" | sudo tee -a /etc/sysctl.conf > /dev/null
@@ -99,7 +98,9 @@ case $choice in
         read -p "Enter additional ports (space-separated): " ports
         sudo ufw allow $ports
         
-        # Reload firewall
+        # Enable and reload firewall
+        echo "Enabling and reloading UFW firewall..."
+        sudo ufw --force enable
         sudo ufw reload
         
         # Add cron job to reload firewall every 30 minutes
